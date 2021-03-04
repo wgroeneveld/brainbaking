1ac06d55db5786a5d8cf0442b41f086b73638bdb
wgroeneveld <wouter@brainbaking.com>
2021-03-04 10:04:25 +0100

    also parse in-reply-to, fix random indieweb tags, use relative date in toot list

--- masto-feed-parser.js
+++ masto-feed-parser.js
@@ -38,6 +38,7 @@ function convertAtomItemToMd(item) {
 
   const mddata = `---
 source: "${item.url}"
+context: "${item.context}"
 title: "${item.title}"
 date: "${item.year}-${item.month}-${item.day}T${strpad(item.date.getHours())}:${strpad(item.date.getMinutes())}:${strpad(item.date.getSeconds())}"
 ---
@@ -60,17 +61,22 @@ ${item.content}
     timeout: 5000,
     retry: 5
   });
-  const root = parser.parse(buffer.toString())
+  const root = parser.parse(buffer.toString(), {
+    ignoreAttributes: false
+  })
   const items = root.feed.entry.map(item => {
     const date = new Date(item.published)
     const year = date.getFullYear()
     const month = strpad(date.getMonth() + 1)
     const day = strpad(date.getDate())
+    // format: <thr:in-reply-to ref='https://social.linux.pizza/users/StampedingLonghorn/statuses/105821099684887793' href='https://social.linux.pizza/users/StampedingLonghorn/statuses/105821099684887793'/>
+    const context = item['thr:in-reply-to'] ? item['thr:in-reply-to']['@_ref'] : ""
 
     return { 
       title: ent.decode(item.title), // summary (cut-off) of content
-      content: ent.decode(item.content), // format: &lt;span class=&quot;h-card.... 
+      content: ent.decode(item.content['#text']), // format: &lt;span class=&quot;h-card.... 
       url: item.id, // format: https://chat.brainbaking.com/objects/0707fd54-185d-4ee7-9204-be370d57663c
+      context,
       id: stripBeforeLastSlash(item.id),
       hash: `${day}h${date.getHours()}m${date.getMinutes()}s${date.getSeconds()}`,
       date, // format: 2021-03-02T16:18:46.658056Z
