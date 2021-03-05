f8569b5ad1079bd44b444d9833fd3ce4da666f15
wgroeneveld <wouter@brainbaking.com>
2021-03-05 12:08:28 +0100

    migrate jamstack scripts to node module, use yarn

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [ '@babel/preset-env' ];
+  const presets = [ 
+  	require('@babel/preset-env')
+  ];
   const plugins = [ ];
 
   return {
---
+++ jamstack-init.js
@@ -0,0 +1,28 @@
+
+const { mastodon, goodreads, lunr } = require('jam-my-stack');
+const fsp = require('fs').promises;
+
+
+(async function() {
+	// 1. parse Mastodon RSS feed
+	console.log("1. parsing Mastodon RSS feed...")
+	await mastodon.parseFeed({
+		notesdir: `${__dirname}/content/notes`,
+		url: "https://chat.brainbaking.com/users/wouter/feed"
+	})
+
+	// 2. update goodreads JS widget
+	console.log("2. Updating Goodreads Widget...")
+	const widget = await goodreads.createWidget("https://www.goodreads.com/review/grid_widget/5451893.Wouter's%20bookshelf:%20read?cover_size=medium&hide_link=&hide_title=&num_books=12&order=d&shelf=read&sort=date_added&widget_id=1496758344")
+	await fsp.writeFile(`${__dirname}/static/js/goodreads.js`, widget, 'utf-8')
+
+	// 3. build Lunr index
+	console.log("3. Building lunr search index...")
+	const index = await lunr.buildIndex([
+		`${__dirname}/content/post`,
+		`${__dirname}/content/notes`])
+	await fsp.writeFile(`${__dirname}/static/js/brainbaking-post.json`, JSON.stringify(index), 'utf-8')
+
+
+	console.log("-- all done!")
+})()
